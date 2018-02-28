89362360b086921f45d2ffd509abf92a1a62917b
hariprasad12994 <hariprasad12994@gmail.com>
2018-02-28 23:12:40 +0530

    Added lib file. May contain core function libraries such as data structures, string operations etc

---
+++ src/arch/x86_64/lib.cpp
@@ -0,0 +1,49 @@
+#include "lib.h"
+#include "types.h"
+
+char* itoa(int num, char* str, int base){
+    int i = 0;
+    bool isNegative = false;
+
+    if(num == 0){
+      str[i++] = '0';
+      str[i] = '\0';
+      return str;
+    }
+
+    if(num < 0 && base == 10){
+      isNegative = true;
+      num = -num;
+    }
+
+    while(num != 0){
+      int rem = num % base;
+      str[i++] = (rem > 9) ? (rem-10) + 'A' : rem + '0' ;
+      num = num/base;
+    }
+
+    if (isNegative)
+      str[i++] = '-';
+
+    str[i] = '\0';
+
+    int stringLength = 0;
+    int stringIndex = 0;
+    while(str[stringIndex] != '\0'){
+      stringLength++;
+      stringIndex++;
+    }
+
+    int leftIndex = 0;
+    int rightIndex = stringLength-1;
+    char temp;
+    while(leftIndex < rightIndex){
+      temp = str[leftIndex];
+      str[leftIndex] = str[rightIndex];
+      str[rightIndex] = temp;
+      leftIndex++;
+      rightIndex--;
+    }
+
+    return str;
+}
