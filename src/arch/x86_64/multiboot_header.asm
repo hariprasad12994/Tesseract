14f73b33af4fa3a1d4e10dc67e5f714fb6672639
hariprasad12994 <hariprasad12994@gmail.com>
2018-02-28 23:18:13 +0530

    Changed header in accordance to grub version 1.0

--- src/arch/x86_64/multiboot_header.asm
+++ src/arch/x86_64/multiboot_header.asm
@@ -1,18 +1,15 @@
+; Our kernel just needs to indicate that it supports Multiboot
+; and every Multiboot-compliant bootloader can boot it.
+;
+MODULEALIGN equ  1<<0                   ; align loaded modules on page boundaries
+MEMINFO     equ  1<<1                   ; provide memory map
+FLAGS       equ  MODULEALIGN | MEMINFO  ; this is the Multiboot 'flag' field
+MAGIC       equ    0x1BADB002           ; 'magic number' lets bootloader find the header
+CHECKSUM    equ -(MAGIC + FLAGS)        ; checksum required
+
 section .multiboot_header
 
-; Our kernel just needs to indicate that it supports Multiboot 
-; and every Multiboot-compliant bootloader can boot it.
-
-header_start:         ; header start label
-  dd 0xe85250d6       ; magic number for grub to detect the OS(multiboot 2)
-  dd 0                ; architecture 0 (protected mode i386)
-  dd header_end - header_start ; header length
-  ; checksum
-  dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
-
-
-  ; end tag
-  dw 0 ; type
-  dw 0 ; flags
-  dd 8 ; size
-header_end:
+align 4
+    dd MAGIC
+    dd FLAGS
+    dd CHECKSUM
