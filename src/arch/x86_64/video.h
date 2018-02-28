736551ff079596768fdc60b7a70104acb14585d3
hariprasad12994 <hariprasad12994@gmail.com>
2018-02-28 23:17:34 +0530

    Introduced print function which behaves like standard printf. Naming changes

--- src/arch/x86_64/video.h
+++ src/arch/x86_64/video.h
@@ -24,19 +24,35 @@ enum Color{
   White
 };
 
-class videoHandler{
+class primitiveVGA{
 private:
   volatile uint16_t* textMemoryPointer;
   uint8_t cursor_x;
   uint8_t cursor_y;
-  uint8_t textColor;
+  uint8_t color;
 
 public:
-  videoHandler();
-  videoHandler(uint8_t foregroundColor, uint8_t backgroundColor);
+  primitiveVGA(uint8_t foregroundColor, uint8_t backgroundColor);
   void clearScreen();
-  void printToScreen(char* string);
+  void printCharToScreen(char ch);
   void scroll();
+  void printFmt(int num, char specifier);
+  void printFmt(const char* str, char specifier);
+  void print(const char* c);
+  template<typename T, typename... Args>
+  void print(const char* s, T value, Args... args){
+    while(*s){
+      if(*s == '%' && *(s+1)!= '%'){
+        char fmt = *(s+1);
+        s++;
+        printFmt(value, fmt);
+        print(++s, args...);
+        return;
+      }
+      else
+        printCharToScreen(*s++);
+    }
+  }
 };
 
 #endif
