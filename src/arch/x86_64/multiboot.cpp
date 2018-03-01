fcf1f3833a439549f156e251d6e99c9b233e41b9
hariprasad12994 <hariprasad12994@gmail.com>
2018-03-02 00:44:38 +0530

    Adding simple panic method and refactoring parsing

--- src/arch/x86_64/multiboot.cpp
+++ src/arch/x86_64/multiboot.cpp
@@ -13,21 +13,23 @@ bool multibootInfo :: checkFlagAvailability(multibootInformationFlag flagType){
 }
 
 void multibootInfo :: parseMultiboot(){
-  // printf("Parsing multiboot structure....\n");
+  primitiveVGA vga((uint8_t)Green, (uint8_t)Black);
+  vga.print("Parsing multiboot structure....\n");
+  vga.print("Reading boot loader name....\n");
   if(multibootInfo :: checkFlagAvailability(BOOTLOADER_NAME)){
-    // printf("Bootloader Name :");
     this->bootloaderName = ((char*)this->ptrToMultibootInfoBlock->boot_loader_name);
+    vga.print("Bootloader name : %s\n", this->bootloaderName);
   }
   else{
-    // Issue an error message, method of handling to be decided
+    panic();
   }
 
+  vga.print("Reading memory map from multiboot structure....\n");
   if(multibootInfo :: checkFlagAvailability(MEMORY_MAP)){
-    // printf("Memory map present\n");
     this->memoryMap = (multibootMemoryMap*)this->ptrToMultibootInfoBlock->mmap_addr;
   }
   else{
-    // Issue an error message, method of handling to be decided
+    panic();
   }
 
   if(multibootInfo :: checkFlagAvailability(ELF_SECTION_HEADER_TABLE)){
@@ -35,4 +37,10 @@ void multibootInfo :: parseMultiboot(){
     this->elfSectionHeaderTable = &(this->ptrToMultibootInfoBlock->u.elf_sec);
   }
   return;
+}
+
+void multibootInfo :: panic(){
+  primitiveVGA vga((uint8_t)Red, (uint8_t)Black);
+  vga.print("Something went wrong....\n");
+  while(1);
 }
