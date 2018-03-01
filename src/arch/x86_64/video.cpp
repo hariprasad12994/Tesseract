83bc222b6c614d27d1a7f86a4410289e89edb2bc
hariprasad12994 <hariprasad12994@gmail.com>
2018-03-02 00:42:14 +0530

    Cursor positions, memory pointer and color code are made static across all objects. Added Documentation

--- src/arch/x86_64/video.cpp
+++ src/arch/x86_64/video.cpp
@@ -1,21 +1,26 @@
 #include "video.h"
 #include "lib.h"
 
+uint8_t primitiveVGA :: cursor_x = 0;
+uint8_t primitiveVGA :: cursor_y = 0;
+uint8_t primitiveVGA :: color = 0;
+volatile uint16_t* primitiveVGA :: textMemoryPointer = (uint16_t*)0xb8000;
+
 primitiveVGA :: primitiveVGA(uint8_t foregroundColor, uint8_t backgroundColor){
-  this->textMemoryPointer = (uint16_t*)0xb8000;
-  this->cursor_x = 0;
-  this->cursor_y = 0;
+  // Higher nibble : backgroundColor, Lower Nibble : foregroundColor
   this->color = (backgroundColor << 4) | (foregroundColor & 0x0F);
 }
 
 void primitiveVGA :: clearScreen(){
-  uint16_t blank = 0x20;
+  uint16_t blank = 0x20; // ASCII(' ') = 0x20
+  // Higher Byte : Colorcode, Lower Byte : Character to written to one byte of VGA memory
   uint16_t charToBeWritten = blank | (this->color << 8);
   uint16_t index;
   uint8_t cursor_x = this->cursor_x;
   uint8_t cursor_y = this->cursor_y;
   for(cursor_y = 0; cursor_y < BUFFER_HEIGHT; cursor_y++){
     for(cursor_x = 0; cursor_x < BUFFER_WIDTH; cursor_x++){
+      // Calculate index based on the x and y position
       index = cursor_y * 80 + cursor_x;
       *(this->textMemoryPointer + index) = charToBeWritten;
     }
