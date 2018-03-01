2ea562904f095f8a6da76e2595f5875ff10aaed3
hariprasad12994 <hariprasad12994@gmail.com>
2018-03-02 00:42:12 +0530

    Cursor positions, memory pointer and color code are made static across all objects. Added Documentation

--- src/arch/x86_64/video.h
+++ src/arch/x86_64/video.h
@@ -3,9 +3,15 @@
 
 #include "types.h"
 
+/*
+Resolution 640 x 400
+*/
 #define BUFFER_HEIGHT 25
 #define BUFFER_WIDTH 80
 
+/*
+Pixel colors possible in basic VGA text mode
+*/
 enum Color{
   Black = 0,
   Blue,
@@ -24,20 +30,39 @@ enum Color{
   White
 };
 
+/*
+Class to handle print to screen operation in basic VGA text mode
+*/
 class primitiveVGA{
 private:
-  volatile uint16_t* textMemoryPointer;
-  uint8_t cursor_x;
-  uint8_t cursor_y;
-  uint8_t color;
-
-public:
-  primitiveVGA(uint8_t foregroundColor, uint8_t backgroundColor);
-  void clearScreen();
+  /*
+  Private data members:
+  Pointer to VGA BIOS memory, x and y position on screen, color code are made
+  static so that they are available across all the vga handler classes
+  */
+  static volatile uint16_t* textMemoryPointer;
+  static uint8_t cursor_x;
+  static uint8_t cursor_y;
+  static uint8_t color;
+  /*
+  Private member function:
+  Printing individual characters to screen, format handling and scrolling
+  functionalities are made internal to the class
+  */
   void printCharToScreen(char ch);
-  void scroll();
   void printFmt(int num, char specifier);
   void printFmt(const char* str, char specifier);
+  void scroll();
+
+public:
+  /*
+  Public member function:
+  A generic print function which behaves like standard printf is implemented using
+  variadic template.
+  Public APIs are provided for printing formatted strings and clearing screen 
+  */
+  primitiveVGA(uint8_t foregroundColor, uint8_t backgroundColor);
+  void clearScreen();
   void print(const char* c);
   template<typename T, typename... Args>
   void print(const char* s, T value, Args... args){
